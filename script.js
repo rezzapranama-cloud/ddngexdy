70c1ce828593f79469c4d2a141f164c530e0f061
muani76 <rezzapranama@gmail.com>
2026-03-19 14:07:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,15 +6,14 @@
 const BIN_ID  = '69bb82b4c3097a1dd53b1183';
 const API_KEY = '$2a$10$EH5wMNiP2eUOvb78.6D0OuBfdNlw.qlmOcZSacTGOYaL9rsup9poq';
 
-const MODS = [
+let MODS = [
   {
     nama:      "SKIN GANGSTA UNDERSHIRT",
     kategori:  "Skin",
-    deskripsi: "Pembuat Ddngexdy 
-Skin bagus untuk Roleplay Gangster maupun biasa",
-    foto:      "foto/skin-gangsta-undershirt.png",
-    file:      "skin/skin/Skin Bagus Dxd 6.7z",
-    ukuran:    "4.6MB",
+    deskripsi: "Pembuat Ddngexdy. Skin bagus untuk Roleplay Gangster maupun biasa.",
+    foto:      "foto/skin-gangsta-undershirt.jpg",
+    file:      "skin/Skin Bagus Dxd 6.7z",
+    ukuran:    "4.6 MB",
     badge:     "badge-new",
   }
 ];
